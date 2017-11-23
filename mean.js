6dde4134e4a2e7534141ac7f34a7b78b2ed01304
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2017-11-22 21:30:01 -0800

    Included container placement

--- mean.js
+++ mean.js
@@ -1,28 +1,46 @@
 const haproxy = require('@quilt/haproxy');
 const Mongo = require('@quilt/mongo');
 const Node = require('@quilt/nodejs');
-const { publicInternet } = require('@quilt/quilt');
+
+const { publicInternet, Container } = require('@quilt/quilt');
 
 function Mean(count, nodeRepo) {
-  const port = 80;
-  this.mongo = new Mongo(count);
-  this.app = new Node({
-    nWorker: count,
-    repo: nodeRepo,
-    env: {
-      PORT: port.toString(),
-      MONGO_URI: this.mongo.uri('mean-example'),
-    },
-  });
+    const port = 80;
+    this.instance_number = count
+    
+    this.mongo = new Mongo(count);
+    this.app = new Node({
+	nWorker: count,
+	repo: nodeRepo,
+	env:{
+	    PORT: port.toString(),
+	    MONGO_URI: this.mongo.uri('mean-example'),
+		},
+    });
+    
+    this.proxy = haproxy.simpleLoadBalancer(this.app.cluster);
+    
+    this.mongo.allowFrom(this.app.cluster, this.mongo.port);
+    this.proxy.allowFrom(publicInternet, haproxy.exposedPort);
 
-  this.proxy = haproxy.simpleLoadBalancer(this.app.cluster);
+    this.exclusive_mongo = function exclusive_mongo(mongo_disk) {
+		for ( i = 0; i < this.instance_number; i++) {
+		    //this.mongo.cluster[i].placeOn({diskSize: mongo_disk[i]});
+		    this.mongo.cluster[i].placeOn(mongo_disk[i]);
+		}
+    }
 
-  this.mongo.allowFrom(this.app.cluster, this.mongo.port);
-  this.proxy.allowFrom(publicInternet, haproxy.exposedPort);
-
-  this.deploy = function deploy(deployment) {
-    deployment.deploy([this.app, this.mongo, this.proxy]);
-  };
+    this.exclusive_node = function exclusive_node(node_disk) {
+		for (i = 0; i < this.instance_number; i++) {
+		    //this.app.cluster[i].placeOn({diskSize: node_disk[i]});
+		    this.app.cluster[i].placeOn(node_disk[i]);
+		}
+    }
+					  
+    
+    this.deploy = function deploy(deployment) {
+	deployment.deploy([this.app, this.mongo, this.proxy]);
+    };
 }
 
 module.exports = Mean;
--- meanExample.js
+++ meanExample.js
@@ -1,26 +1,50 @@
 const quilt = require('@quilt/quilt');
-const Mean = require('./mean.js');
-const utils = require('./utils.js');
+const Mean = require('./mean');
+const utils = require('./utils');
 
 // Replication to use for the node application
 // and Mongo.
 const count = 2;
 const infrastructure = quilt.createDeployment(
-{namespace: "tsaianson-mean"}
+    {namespace: "ansonmean"}
 );
 
-const machine = new quilt.Machine({
-  provider: 'Amazon',
-  size: "m4.large",
-  preemptible: true,
-  sshKeys: quilt.githubKeys("TsaiAnson")
+const machine0 = new quilt.Machine({
+    provider: 'Amazon',
+    size: "m4.large",
+    preemptible: true,
 });
 
-utils.addSshKey(machine);
+const machine1 = new quilt.Machine({
+    provider: 'Amazon',
+    size: "m4.large",
+    preemptible: true,
+    diskSize: 12,
+});
 
-infrastructure.deploy(machine.asMaster());
-infrastructure.deploy(machine.asWorker().replicate(count));
+const machine2 = new quilt.Machine({
+    provider: 'Amazon',
+    size: "m4.large",
+    preemptible: true,
+    diskSize: 13,
+});
+
+utils.addSshKey(machine0);
+utils.addSshKey(machine1);
+utils.addSshKey(machine2);
+
+infrastructure.deploy(machine0.asMaster());
+
+infrastructure.deploy(machine1.asWorker());
+infrastructure.deploy(machine2.asWorker());
 
 const nodeRepository = 'https://github.com/TsaiAnson/node-todo.git';
 const mean = new Mean(count, nodeRepository);
+
+var mongo_placements = [machine1,machine2];
+var node_placements = [machine1,machine2];
+
+mean.exclusive_mongo(mongo_placements);
+mean.exclusive_node(node_placements);
+
 infrastructure.deploy(mean);
